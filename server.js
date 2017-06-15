a6c2393cda54a52ac1e84e2c194602fbbf0ce986
Sebastian Feurer <sebastian.feurer@pb.com>
2017-06-15 11:15:11 +0200

    Second commit

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const hbs = require('hbs')
 const fs = require('fs')
 var app = express();
 
+const port = (process.env.PORT || 3000);
+
 hbs.registerPartials(__dirname+'/views/partials')
 app.set('view engine', 'hbs')
 app.use((req, res, next) => {
@@ -57,6 +59,6 @@ app.get('/bad', (req, res) =>{
   })
 })
 
-app.listen(3000, () => {
-  console.log('Sever is up on port 3000');
+app.listen(port, () => {
+  console.log(`Sever is up on port ${port}`);
 })
